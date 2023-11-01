ba86c931d739910c9bfad56dc2a08b40560a13a4
dan-albuquerque <daniloalbuquerquemelo@gmail.com>
2023-11-01 09:40:06 -0300

    funcao de printforca

--- library.c
+++ library.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "library.h"
 
 void pausa() {
     printf("\n\nPressione ENTER para continuar...\n\n");
@@ -16,11 +17,13 @@ void menu() {
 
 void jogoSolo(char *nome) {
     printf("Seja bem vindo %s!\n", nome);
+    desenhaForca();
 }
 
 void jogoDupla(char *j1, char *j2) {
     printf("Jogador 1: %s\n", j1);
     printf("Jogador 2: %s\n", j2);
+    desenhaForca();
 }
 
 void printSobre(){
@@ -28,4 +31,32 @@ void printSobre(){
     printf("Desenvolvedores\n");
     printf("Danilo Albuquerque de Melo:\t dam@cesar.school\n");
     printf("Paulo Montenegro Campos:\t pmc3@cesar.school\n");
+}
+
+
+void desenhaForca() {
+
+    int erros = 0;
+
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros>=1?'(':' '), (erros>=1?'_':' '), (erros>=1?')':' '));
+    printf(" |      %c%c%c  \n", (erros>=3?'\\':' '), (erros>=2?'|':' '), (erros>=3?'/': ' '));
+    printf(" |       %c     \n", (erros>=2?'|':' '));
+    printf(" |      %c %c   \n", (erros>=4?'/':' '), (erros>=4?'\\':' '));
+    printf(" |              \n");
+    printf("_|___           \n");
+    printf("\n\n");
+/*
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }
+    printf("\n");
+*/
 }
--- library.h
+++ library.h
@@ -6,5 +6,6 @@ void menu();
 void jogoSolo(char *nome);
 void jogoDupla(char *j1, char *j2);
 void printSobre();
+void desenhaForca();
 
 #endif
